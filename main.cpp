1c292ae66b14ab08165bb48563f3d0fa10065375
Rolandas55 <57255127+Rolandas55@users.noreply.github.com>
2020-02-12 18:49:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main(){
-  cout << "Hello world" << endl;
-  return 0;
+    string pirma, antra, trecia, vard;
+    int ilg;
+    cin >> vard;
+    ilg = vard.length()+14;
+    pirma.append(ilg, '*');
+    antra = "*";
+    antra.append(ilg-2, ' ');
+    antra.append("*");
+    trecia = "* Sveikas, " + vard + "! *";
+    cout << pirma << endl << antra << endl << trecia << endl << antra  << endl << pirma;
+    return 0;
 }
