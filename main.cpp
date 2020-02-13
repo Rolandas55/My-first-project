f800dab76f5e2afad337d8d39646fc782159a04b
Rolandas55 <57255127+Rolandas55@users.noreply.github.com>
2020-02-13 14:49:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    string pirma, antra, trecia, vard;
+    string pirma, antra, treciav, treciam, vard, r;
     int ilg;
     cin >> vard;
-    ilg = vard.length()+14;
+    ilg=vard.length()+14;
     pirma.append(ilg, '*');
-    antra = "*";
+    antra = '*';
     antra.append(ilg-2, ' ');
     antra.append("*");
-    trecia = "* Sveikas, " + vard + "! *";
-    cout << pirma << endl << antra << endl << trecia << endl << antra  << endl << pirma;
+    treciav = "* Sveikas, " + vard + "! *";
+    treciam = "* Sveika, " + vard + "!  *";
+    r=vard.back();
+    cout << r;
+    if(r=="e" or r=="a")
+    cout << pirma << endl << antra << endl << treciam << endl << antra  << endl << pirma;
+    else cout << pirma << endl << antra << endl << treciav << endl << antra  << endl << pirma;
     return 0;
 }
